bce2eeab079fc9aee0726f89cf86e8f2eabc38fa
민지영 <kuck523@gmail.com>
2024-12-28 14:50:25 +0900

    delete sevice.js and final

--- mission04/APIS/ArticleService.js
+++ mission04/APIS/ArticleService.js
@@ -1,28 +1,85 @@
-import { getList, getItem, createItem, patchItem, deleteItem } from "./service.js";
+import apiMission from "./axiosDefault.js";
+import { isEmpty } from "./checkError.js";
+import { REQUIRED_FIELDS } from "./constants.js";
+import { verifyData } from "./checkError.js";
 
-const baseUrl = `https://sprint-mission-api.vercel.app/articles`;
+const endPoint = "articles";
 
-export const getArticleList = (params) => {
-  // 게시글 목록 조회
-  return getList(baseUrl, params);
+const initParams = (params) => {
+  // GET method 파라미터 초기화
+  const defaultParams = {
+    page: 1,
+    pageSize: 1000,
+  };
+
+  params.page = isEmpty(params.page) ? defaultParams.page : params.page;
+  params.pageSize = isEmpty(params.pageSize) ? defaultParams.pageSize : params.pageSize;
+
+  return params;
+};
+
+export const getArticleList = async (params) => {
+  // 상품 목록 조회
+  initParams(params); // 파라미터 값이 없을 시 초기화
+
+  try {
+    const url = endPoint;
+    const response = await apiMission.get(url, { params });
+    const result = response.data;
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
 export const getArticle = async (id) => {
-  // 게시글 상세 조회
-  return getItem(baseUrl, id);
+  // 상품 상세 조회
+  try {
+    const url = endPoint + `/${id}`;
+    const response = await apiMission.get(url);
+    const result = response.data;
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
-export const createArticle = async (params) => {
-  // 게시글 등록
-  return createItem(baseUrl, params);
+export const createArticle = async (data) => {
+  // 상품 등록
+  const fields = REQUIRED_FIELDS["POST"][endPoint]; // method에 해당하는 필수 필드 가져오기
+  verifyData(fields, data); // data 검증
+
+  try {
+    const url = endPoint;
+    const response = await apiMission.post(url, data);
+    const result = response.data;
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
-export const patchArticle = async (id, params) => {
+export const patchArticle = async (id, data) => {
   // 게시글 수정
-  return patchItem(baseUrl, id, params);
+  const fields = REQUIRED_FIELDS["PATCH"][endPoint]; // method에 해당하는 필수 필드 가져오기
+  verifyData(fields, data); // data 검증
+
+  try {
+    const url = endPoint + `/${id}`;
+    const response = await apiMission.patch(url, data);
+    const result = response.data;
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
-export const deleteArticle = (id) => {
+export const deleteArticle = async (id) => {
   // 게시글 삭제
-  deleteItem(baseUrl, id);
+  try {
+    const url = endPoint + `/${id}`;
+    await apiMission.delete(url);
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
--- mission04/main.js
+++ mission04/main.js
@@ -1,41 +1,41 @@
-// import { getArticleList, getArticle, createArticle, patchArticle, deleteArticle } from "./APIS/ArticleService.js";
+import { getArticleList, getArticle, createArticle, patchArticle, deleteArticle } from "./APIS/ArticleService.js";
 import { getProductList, getProduct, createProduct, patchProduct, deleteProduct } from "./APIS/ProductService.js";
 
 // articles
-// let articleList = await getArticleList({
-//   page: 0,
-//   pageSize: 0,
-//   keyword: "내용",
-// });
-// console.log("articleList: ", articleList);
+let articleList = await getArticleList({
+  page: 0,
+  pageSize: 0,
+  keyword: "내용",
+});
+console.log("articleList: ", articleList);
 
-// let article = await createArticle({
-//   title: "5기 파트2 팀 배정 안내",
-//   content: `1팀 (심정욱 멘토님) : 최은비,정한샘,정하윤,가승연,이동혁,최유리\n2팀 (김현웅 멘토님) : 박세정,유호은,최수빈,김조순,봉재완,윤민호
-// \n3팀 (류제천 멘토님) : 민지영,김희성,신진호,김민희,김승우`,
-//   image: "img-url-1",
-// });
-// console.log("create article: ", article);
+let article = await createArticle({
+  title: "5기 파트2 팀 배정 안내",
+  content: `1팀 (심정욱 멘토님) : 최은비,정한샘,정하윤,가승연,이동혁,최유리\n2팀 (김현웅 멘토님) : 박세정,유호은,최수빈,김조순,봉재완,윤민호
+\n3팀 (류제천 멘토님) : 민지영,김희성,신진호,김민희,김승우`,
+  image: "img-url-1",
+});
+console.log("create article: ", article);
 
-// article = await patchArticle(1416, {
-//   title: "5기 파트2 팀 배정 안내 (민지영)",
-//   content: `3팀 (류제천 멘토님) : 민지영,김희성,신진호,김민희,김승우`,
-//   image: "img-url-1",
-// });
-// console.log("patch article: ", article);
+article = await patchArticle(1416, {
+  title: "5기 파트2 팀 배정 안내 (민지영)",
+  content: `3팀 (류제천 멘토님) : 민지영,김희성,신진호,김민희,김승우`,
+  image: "img-url-1",
+});
+console.log("patch article: ", article);
 
-// deleteArticle(1416);
+deleteArticle(1416);
 
-// article = await getArticle(1416);
-// console.log("get article: ", article);
+article = await getArticle(1416);
+console.log("get article: ", article);
 
 // products
-// let productList = await getProductList({
-//   page: 0,
-//   pageSize: 0,
-//   keyword: "상품",
-// });
-// console.log("productList: ", productList);
+let productList = await getProductList({
+  page: 0,
+  pageSize: 0,
+  keyword: "상품",
+});
+console.log("productList: ", productList);
 
 const productData = {
   name: "도서) 리팩토링 2판",
@@ -48,21 +48,18 @@ const productData = {
 let product = await createProduct(productData);
 console.log("create product: ", product);
 
-// const patchedProductData = {
-//   name: "도서) 리팩토링 2판 / 가격 수정",
-//   description: "",
-//   price: 40000,
-//   manufacturer: "한빛미디어",
-//   tags: ["자바스크립트", "리팩토링", "도서"],
-//   images: ["이미지-url-1", "이미지-url-2"],
-// };
+const patchedProductData = {
+  name: "도서) 리팩토링 2판 / 가격 수정",
+  description: "",
+  price: 40000,
+  manufacturer: "한빛미디어",
+  tags: ["자바스크립트", "리팩토링", "도서"],
+  images: ["이미지-url-1", "이미지-url-2"],
+};
+let patchedProduct = await patchProduct(713, patchedProductData);
+console.log("patch product: ", patchedProduct);
 
-// let patchedProduct = await patchProduct(713, patchedProductData);
-// console.log("patch product: ", patchedProduct);
+delete deleteProduct(714);
 
-// delete
-// deleteProduct(714);
-
-// get
-// let product = await getProduct(713);
-// console.log("get product: ", product);
+product = await getProduct(713);
+console.log("get product: ", product);
